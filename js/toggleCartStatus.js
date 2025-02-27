03ee98b6f1eb623d43476d77988ad135bc5c1c06
Tatiana Anderson <happytasha9933@gmail.com>
2025-02-27 20:10:34 +0400

    add logo anmation and form confirmation

--- js/toggleCartStatus.js
+++ js/toggleCartStatus.js
@@ -4,10 +4,6 @@ logo.addEventListener('click', function(){
     location.reload();
 })
 
-window.addEventListener('scroll', function(){
-    const scrollPosition = this.window.scrollY
-     ogo.style.left = `${scrollPosition % (window.innerWidth - 100)}px`
-})
 
 
 
@@ -26,4 +22,39 @@ function toggleCartStatus(){
         cartEmptyBadge.classList.remove('none');
         orderForm.classList.add('none');
     }
+    
 }
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    const phoneInput = document.querySelector('#phone');
+    const errorMessage = document.querySelector('#error-message');
+
+
+    phoneInput.addEventListener('input', function(event){
+        let valueInput = phoneInput.ariaValueMax.replace(/\D/g, '');
+
+        if (valueInput > 10) {
+            valueInput = valueInput.slice(0, 10);
+        
+        if (inputValue.length > 1) {
+            inputValue = '+7 (' + inputValue.slice(1, 4) + ') ' + inputValue.slice(4, 7) + '-' + inputValue.slice(7, 9) + '-' + inputValue.slice(9, 11);
+            } else if (inputValue.length > 0) {
+            inputValue = '+7 (' + inputValue.slice(1, 4);
+            }
+            phoneInput.value = inputValue; // Обновляем поле ввода
+
+    // Очищаем сообщение об ошибке, если ввод правильный
+        errorMessage.textContent = '';
+        }
+    });
+
+    document.getElementById('order-form').addEventListener('submit', function(event) {
+        const phoneValue = phoneInput.value.replace(/\D/g, ''); // Убираем все символы, не являющиеся цифрами
+    
+        if (phoneValue.length !== 10) {
+            event.preventDefault(); // Отменяем отправку формы, если номер неправильный
+            errorMessage.textContent = 'Пожалуйста, введите правильный номер телефона.';
+        }
+    });
+});
